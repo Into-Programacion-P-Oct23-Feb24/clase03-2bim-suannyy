98b0cbd85ad1307464bfdb506893fa24137b98b4
suannyy <sjmayon1@utpl.edu.ec>
2023-12-20 16:14:03 -0500

    Ejercicio001

--- Ejemplosc3/src/paquete01/Ejemplo022.java
+++ Ejemplosc3/src/paquete01/Ejemplo022.java
@@ -4,13 +4,15 @@
  * and open the template in the editor.
  */
 package paquete01;
+
 import java.util.Scanner;
 import java.util.Locale;
+
 /**
  *
  * @author reroes
  */
-public class Ejemplo02 {
+public class Ejemplo022 {
 
     /**
      * @param args the command line arguments
@@ -18,32 +20,35 @@ public class Ejemplo02 {
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
         Scanner entrada = new Scanner(System.in);
-        
+        entrada.useLocale(Locale.US);
+
         // forma 1
         int[][] arreglo1 = new int[3][4]; //
         System.out.printf("Uso de la opción length: %d\n", arreglo1.length);
-        
+
         System.out.printf("Valor en la posición [0][2]\n", arreglo1[0][2]);
-       
+
         arreglo1[0][2] = 1000;
         for (int f = 0; f < arreglo1.length; f++) {
             for (int c = 0; c < arreglo1[f].length; c++) {
                 System.out.printf("ingresar valor de la posicion [%d][%d]\n",
-                        f,c);
+                        f, c);
                 int numero = entrada.nextInt();
-                arreglo1[f][c] = numero;
+                if (numero >= 10 && numero % 2 == 0) {
+                    arreglo1[f][c] = 0;
+                } else {
+                    arreglo1[f][c] = numero;
+                }
             }
-        }
-               
-        
-               
-        for (int fila = 0; fila < arreglo1.length; fila++) {
-            for (int col = 0; col < arreglo1[fila].length; col++) {
-                System.out.printf("fila[%d] columna[%d] = %d\n", 
-                        fila, col, arreglo1[fila][col]);
+
+            for (int fila = 0; fila < arreglo1.length; fila++) {
+                for (int col = 0; col < arreglo1[fila].length; col++) {
+                    System.out.printf("fila[%d] columna[%d] = %d\n",
+                            fila, col, arreglo1[fila][col]);
+                }
             }
+
         }
-        
+
     }
-    
 }
--- Ejemplosc3/src/paquete05/ejemplo001.java
+++ Ejemplosc3/src/paquete05/ejemplo001.java
@@ -8,7 +8,7 @@ package paquete05;
  *
  * @author LAB.ELECT
  */
-public class ejemplo001 {
+public class Ejemplo001 {
 
     public static void main(String[] args) {
         int[] arreglo = new int[2];
